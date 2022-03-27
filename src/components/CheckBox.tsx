9048e94f50d6df826bb262a2b0b36675a040075f
iskaa02 <eaemk@outlook.sa>
2022-03-27 22:26:59 +0200

    added shadows to buttons and some views

---
+++ src/components/CheckBox.tsx
@@ -0,0 +1,45 @@
+import { Feather } from "@expo/vector-icons";
+import { MotiView } from "moti";
+import { Icon, Pressable } from "native-base";
+import * as React from "react";
+
+type CheckBoxProps = {
+  value: boolean;
+  setValue: React.Dispatch<React.SetStateAction<boolean>>;
+  color: string;
+};
+const CheckBox = ({ value, setValue, color }: CheckBoxProps) => {
+  return (
+    <Pressable
+      hitSlop={20}
+      w="20px"
+      h="20px"
+      borderRadius={10}
+      borderWidth={value ? 0 : 2}
+      style={{ marginEnd: 18 }}
+      borderColor={color}
+      justifyContent="center"
+      alignItems="center"
+      onPress={() => setValue(i => !i)}
+    >
+      <MotiView
+        style={{
+          backgroundColor: color,
+          width: "105%",
+          height: "105%",
+          justifyContent: "center",
+          alignItems: "center",
+          borderRadius: 10,
+        }}
+        animate={{
+          scale: value ? 1 : 0.3,
+          opacity: value ? 1 : 0,
+        }}
+      >
+        <Icon as={<Feather name="check" />} color="em.10" size="14px" />
+      </MotiView>
+    </Pressable>
+  );
+};
+
+export default CheckBox;
