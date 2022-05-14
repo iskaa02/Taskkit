67c81431b34be6c2c523a7f1ba1e4e0040f2a9a0
iskaa02 <eaemk@outlook.sa>
2022-05-14 22:54:29 +0200

    Change the way to edit description

--- src/navigation/bottomTabs/lists/task/TaskScreen.tsx
+++ src/navigation/bottomTabs/lists/task/TaskScreen.tsx
@@ -14,12 +14,16 @@ import dayjs from "dayjs";
 import {
   Box,
   Heading,
+  Input,
   KeyboardAvoidingView,
+  Pressable,
   ScrollView,
+  Text,
   useColorModeValue,
 } from "native-base";
 import * as React from "react";
 import { useTranslation } from "react-i18next";
+import { TextInput } from "react-native";
 import Autolink from "react-native-autolink";
 import EditHeaderButton from "../EditHeaderButton";
 import { EditTaskSheet } from "./EditTaskSheet";
@@ -104,7 +108,13 @@ const RawScreen = ({ navigation, route, task }: TaskScreenProps) => {
             </Heading>
           </Box>
         </Box>
-        <Description accent={accent} text={task.description} />
+        <Description
+          onChange={description => {
+            task.editTask({ description });
+          }}
+          accent={accent}
+          text={task.description}
+        />
         <Box px="20px">
           <Box mt="4">
             {!!task.reminder?.valueOf() ? (
@@ -166,21 +176,89 @@ const Screen = withDB<TaskScreenProps, { task: Task }>(
 
 type DescriptionProps = {
   text: string;
+  onChange: (n: string) => void;
   accent: string;
 };
-const Description = ({ text, accent }: DescriptionProps) => {
+const Description = ({ text, accent, onChange }: DescriptionProps) => {
   const defaultTextColor = useColorModeValue("#fff", "#000");
-  if (!text) return null;
+  const [editable, setEditable] = React.useState(false);
+  const inputRef = React.useRef<TextInput>(null);
+  const { t } = useTranslation();
+  React.useEffect(() => {
+    if (editable) inputRef.current?.focus();
+  }, [editable]);
+  if (!text && !editable)
+    return (
+      <Box px="20px" py="15px">
+        <Pressable
+          onPress={() => {
+            setEditable(true);
+          }}
+          _pressed={{ opacity: 0.7 }}
+          w="100%"
+          py="2"
+          rounded="lg"
+          borderStyle="dashed"
+          borderWidth={2}
+          justifyContent="center"
+          alignItems="center"
+          borderColor="em.1:alpha.70"
+          bg="em.1:alpha.10"
+        >
+          <Text fontSize="lg">{t("add") + " " + t("description")}</Text>
+        </Pressable>
+      </Box>
+    );
+
   return (
-    <Box px="20px" py="3" bg={accent}>
-      <Autolink
-        textProps={{ style: { fontSize: 18, color: defaultTextColor } }}
-        linkStyle={{
-          fontSize: 18,
-          textDecorationLine: "underline",
-        }}
-        text={text}
-      />
-    </Box>
+    <Pressable
+      px="15px"
+      py="10px"
+      bg={accent}
+      onLongPress={() => {
+        setEditable(true);
+      }}
+    >
+      <Box
+        px="5px"
+        borderColor={editable ? "em.10:alpha.30" : "transparent"}
+        rounded="md"
+        borderWidth={1}
+      >
+        {editable ? (
+          <Input
+            fontSize={18}
+            ref={inputRef}
+            variant="unstyled"
+            textAlign="left"
+            selectionColor="em.10"
+            color={defaultTextColor}
+            multiline
+            defaultValue={text}
+            onEndEditing={i => {
+              setEditable(false);
+              onChange(i.nativeEvent.text);
+            }}
+            p="0"
+            m="0"
+          />
+        ) : null}
+        {editable ? null : (
+          <Autolink
+            textProps={{
+              style: {
+                fontSize: 18,
+                color: defaultTextColor,
+              },
+            }}
+            linkStyle={{
+              fontSize: 18,
+              textDecorationLine: "underline",
+            }}
+            text={text}
+          />
+        )}
+      </Box>
+    </Pressable>
   );
 };
